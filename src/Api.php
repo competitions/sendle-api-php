8bd653c15a1b59ff8d517d39dfe1e976f29eb46f
Zway Yee <zwayyee@Zways-MacBook-Pro.local>
2018-02-26 16:07:45 +1100

    big push

--- src/Api.php
+++ src/Api.php
@@ -1,19 +1,62 @@
 <?php
+
+
 namespace Sendle;
+
 class Client {
-    public static function HTTPRequest($url, $postdata = NULL) {
-        $ch = curl_init();
 
-        if ($postdata !== NULL) {
+    public function quote()
+    {
+    }
+
+    public static function HTTPRequest($api_endpoint, $tasks, $postdata,$sendleId,$apiKey) {
+
+        $endpointsarray = [
+            'sendle-sandbox'        => 'https://'.$sendleId.':'.$apiKey.'@sandbox.sendle.com',
+            'sendle-prod'        => 'https://api.sendle.com',
+           
+
+        ];
+        
+        $pathsarray = [
+            'ping' => '/api/ping',
+            'order' => '/api/orders',
+            'quote' => '/api/quote'
+        ];
+        
+        $tasksarray = [
+            'ping' => 'GET',
+            'order' => 'POST',
+            'quote' => 'GET'
+        ];
+        
+        
+        
+        $url = $endpointsarray[$api_endpoint] . $pathsarray[$tasks];
+        if ($pathsarray[$tasks] == '/api/quote') {
+            $url = 'https://api.sendle.com/api/quote?pickup_suburb='.rawurlencode($postdata["pickup_suburb"]).'&pickup_postcode='.$postdata["pickup_postcode"].'&delivery_suburb='.rawurlencode($postdata["delivery_suburb"]).'&delivery_postcode='.$postdata["delivery_postcode"].'&kilogram_weight='.$postdata["kilogram_weight"].'&cubic_metre_volume='.$postdata["cubic_metre_volume"];
+            echo $url;
+            $ch = curl_init($url);
+        }
+        else {
+            $ch = curl_init($url);
+
+        }
+        //Tell cURL that we want to send a POST request.
+        if ($tasksarray[$tasks] == 'POST') {
+            curl_setopt($ch, CURLOPT_POST, 1);
+        }
+        //Attach our encoded JSON string to the POST fields.
+
+        if ($tasksarray[$tasks] == 'POST') {
             curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-            $headers[] = 'Content-Type: application/json';
-          }
-
-    curl_setopt_array($ch, [
-    
-        CURLOPT_POST, 1,
-        CURLOPT_URL => $url,
-      ]);
+        }
+        
+        //Set the content type to application/json
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+        
+        //Execute the request
+       
       $rawresult = curl_exec($ch);
       $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
       $res_headers = substr($rawresult, 0, $header_size);
@@ -22,4 +65,7 @@ class Client {
       curl_close($ch);
       return [$code, $result];
 }
+
+
+
 }
