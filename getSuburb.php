4a9015d84a293b61772e2a1cfcd13cb423e978f1
Zway Yee <zwayyee@Zways-MacBook-Pro.local>
2018-03-02 12:57:33 +1100

    update readme

--- getSuburb.php
+++
@@ -1,6 +0,0 @@
-<?php
-$term = $_GET['term'];
-$temp = array("api_key"=>"{removed}","term"=>$term, "state"=>"nsw","max_results"=>5);
-$query = http_build_query($temp);
-$list = file_get_contents("http://api.addressify.com.au/address/suburbAutoComplete?$query");
-echo $list;
